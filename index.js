41d7a39dba86dd66b929d758c7db4cf173237ac8
krzkro4122 <75375838+krzkro4122@users.noreply.github.com>
2021-12-03 23:09:46 +0100

    pathik

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // --- IMPORTS --- //
 
-import * as THREE from './node_modules/three';
-
-import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls';
-
+import * as THREE from './node_modules/three/build/three.module.js';
 
 // --- MAIN INITIALIZATIONS --- //
 
